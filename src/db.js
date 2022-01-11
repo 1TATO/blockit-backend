97a4f7ed2e3e951bb885517fe4ee693a9b056430
1TATO <josenetopr5@gmail.com>
2022-01-11 15:05:06 -0300

    Created Product Model

---
+++ src/db.js
@@ -0,0 +1,40 @@
+const mysql = require('mysql2');
+const Sequelize = require("sequelize");
+require("dotenv/config");
+
+const dbName = process.env.DB_NAME;
+const dbUser = process.env.DB_USER;
+const dbHost = process.env.DB_HOST;
+const dbPassword = process.env.DB_PASSWORD;
+const dbDialect = process.env.DB_DIALECT;
+
+const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
+  dialect: dbDialect,
+  host: dbHost,
+});
+
+const db = mysql.createConnection({
+  host: dbHost,
+  user: dbUser,
+  password: dbPassword,
+  database: dbDialect
+});
+
+db.connect((err) => {
+  if (err) {
+    console.log(err)
+    console.log('Erro ao conectar ao banco de dados');
+    return;
+  }
+
+  console.log('Conexao estabelecida');
+});
+
+// try {
+//   await sequelize.authenticate();
+//   console.log('Connection has been established successfully.');
+// } catch (error) {
+//   console.error('Unable to connect to the database:', error);
+// }
+
+module.exports = sequelize;
---
+++ src/models/Product.js
@@ -0,0 +1,41 @@
+const DataTypes = require("sequelize");
+const db = require("../db");
+
+const Product = db.define('product', {
+  id: {
+    type: DataTypes.INTEGER.UNSIGNED,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  price: {
+    type: DataTypes.NUMBER,
+    allowNull: false,
+  },
+  imageUrl: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  launchDate: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+  developer: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  distributor: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  }
+});
+
+module.exports = Product;
